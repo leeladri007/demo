fb496fbaf346092461675f891a044509430c277f
ldommala <ldommala@LIN35007366.corp.capgemini.com>
2021-05-17 19:27:34 +0530

    first commit

---
+++ GitDemo/src/com/gitdemo/Test.java
@@ -0,0 +1,15 @@
+package com.gitdemo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String lee="yes";
+		
+		if(lee=="yes") {
+			System.out.println("this is correct");
+		}
+		else {
+			System.out.println("this is incorrect");
+		}
+	}
+}
